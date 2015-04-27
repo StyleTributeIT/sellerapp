fc731f257b58bc2eb2768e1d9ca0d2e2d5e17f27
Selim Mustafaev <selim.app@zuzex.com>
2015-04-27 10:59:19 +0300

    Initial Commit

---
+++ StyleTribute/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StyleTribute
+//
+//  Created by Selim Mustafaev on 27/04/15.
+//  Copyright (c) 2015 Selim Mustafaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StyleTribute/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StyleTribute
+//
+//  Created by Selim Mustafaev on 27/04/15.
+//  Copyright (c) 2015 Selim Mustafaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
