3387e3b2a1a702a53257816cf6516a3a9f3fc592
Selim Mustafaev <selim@zuzex.com>
2015-06-16 14:04:35 +0300

    removed fix for image paths

--- StyleTribute/ImageType.m
+++ StyleTribute/ImageType.m
@@ -19,15 +19,8 @@
     
     imgType.name = [[self class] parseString:@"name" fromDict:dict];
     imgType.type = [[self class] parseString:@"type" fromDict:dict];
-    
     imgType.preview = [[self class] parseString:@"preview_image" fromDict:dict];
     imgType.outline = [[self class] parseString:@"outline_image" fromDict:dict];
-    
-    // FIXME: remove replacing after backend fix
-    if([imgType.preview rangeOfString:@"catalog/category"].location == NSNotFound) {
-        imgType.preview = [imgType.preview stringByReplacingOccurrencesOfString:@"media" withString:@"media/catalog/category"];
-        imgType.outline = [imgType.outline stringByReplacingOccurrencesOfString:@"media" withString:@"media/catalog/category"];
-    }
     
     return imgType;
 }
