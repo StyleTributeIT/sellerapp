bfa9e09c5c7570d4480851f6f445bcaf57f0f222
Claudio Mettler <github@ponyfleisch.ch>
2015-11-18 10:50:06 +0800

    progress

--- StyleTribute/GlobalDefs.h
+++ StyleTribute/GlobalDefs.h
@@ -13,7 +13,7 @@
 #import <TestFairy.h>
 
 // static NSString* const DefApiHost		= @"http://test-api.styletribute.com/";
-// static NSString* const DefApiHost		= @"http://magentolocal.ctrlm.sg/Magento-on-Angular/api/public/";
+//static NSString* const DefApiHost		= @"http://magentolocal.ctrlm.sg/Magento-on-Angular/api/public/";
 //static NSString* const DefApiHost       = @"http://magapi.evgeniy.lara/";
 static NSString* const DefApiHost		= @"https://api.styletribute.com/";
 
