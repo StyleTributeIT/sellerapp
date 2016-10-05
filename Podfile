ad06e3a1beb40a1431631cdec098247478e270d2
Claudio Mettler <github@ponyfleisch.ch>
2016-10-05 13:08:41 +0800

    cleanup build setup

--- Podfile
+++ Podfile
@@ -1,6 +1,6 @@
 platform :ios, '7.0'
 
-pod 'AFNetworking'
+pod 'AFNetworking', '~> 2.6'
 pod 'TestFairy'
 pod 'Reachability'
 pod 'KASlideShow'
--- Pods/Target Support Files/Pods-StyleTributeTests-KIF/Pods-StyleTributeTests-KIF-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_StyleTributeTests_KIF : NSObject
-@end
-@implementation PodsDummy_Pods_StyleTributeTests_KIF
-@end
