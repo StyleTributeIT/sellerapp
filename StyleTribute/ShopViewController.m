261bcec89762fe6ddcc81c5b12e4186beb2b28aa
Vasilkoff <dev.vasilkoff@gmail.com>
2016-11-20 00:04:00 +0200

    loading shop url

--- StyleTribute/ShopViewController.m
+++ StyleTribute/ShopViewController.m
@@ -19,6 +19,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [GlobalHelper addLogoToNavBar:self.navigationItem];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://styletribute.com/"]];
+    [self.webView loadRequest:request];
 }
 
 - (void)didReceiveMemoryWarning {
