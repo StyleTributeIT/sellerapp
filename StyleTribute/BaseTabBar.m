72675394f1ec206a1f68c086053fc9a756ce8ad4
Vasilkoff <dev.vasilkoff@gmail.com>
2016-12-14 01:03:57 +0200

    fixed App-wide

--- StyleTribute/BaseTabBar.m
+++ StyleTribute/BaseTabBar.m
@@ -67,7 +67,7 @@ NSInteger CENTER_BTN_IDX = 3;
 {
     UIViewController* viewController = [[UIViewController alloc] init];
     viewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:title image:image tag:0];
-    viewController.tabBarItem.imageInsets = UIEdgeInsetsMake(47, 0, -47, 0);
+    viewController.tabBarItem.imageInsets = UIEdgeInsetsMake(57, 0, -57, 0);
     
     return viewController;
 }
