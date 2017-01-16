ebc881a8d85b87db64e713c6ce39765ab6fa089f
gibtang <gibtang@gmail.com>
2017-01-16 22:44:21 +0800

    If the user enters their own price, the wrong API is called still

--- StyleTribute/EnterPriceViewController.m
+++ StyleTribute/EnterPriceViewController.m
@@ -64,15 +64,17 @@
         if(self.priceField.text.length > 0 && !self.isInProgress) {
             self.isInProgress = YES;
             [MRProgressOverlayView showOverlayAddedTo:[UIApplication sharedApplication].keyWindow title:@"Loading..." mode:MRProgressOverlayViewModeIndeterminate animated:YES];
-            [[ApiRequester sharedInstance] getPriceSuggestionForProduct:[DataCache getSelectedItem] andOriginalPrice:[self.priceField.text floatValue] success:^(float priceSuggestion) {
+            Product *p = [DataCache getSelectedItem];
+            float price = [self.priceField.text floatValue];
+             [[ApiRequester sharedInstance] getSellerPayoutForProduct:p.category.idNum price:price success:^(float priceSuggestion) {
+          //  [[ApiRequester sharedInstance] getPriceSuggestionForProduct:[DataCache getSelectedItem] andOriginalPrice:[self.priceField.text floatValue] success:^(float priceSuggestion) {
                 self.earnLabel.textColor = [UIColor colorWithRed:1.f green:64/255.f blue:140/255.f alpha:1.f];
                 self.enterPriceLabel.textColor = [UIColor colorWithRed:1.f green:64/255.f blue:140/255.f alpha:1.f];
                 self.earnPriceView.layer.borderColor = [UIColor colorWithRed:1.f green:64/255.f blue:140/255.f alpha:1.f].CGColor;
                 self.priceEarned.text = [NSString stringWithFormat:@"%.2f", priceSuggestion];
                 self.priceEarned.textColor = [UIColor colorWithRed:1.f green:64/255.f blue:140/255.f alpha:1.f];
                 Product *p = [DataCache getSelectedItem];
-               // p.price = [self.priceEarned.text floatValue];
-                p.suggestedPrice = priceSuggestion;
+                p.price = price;
                 [DataCache setSelectedItem:p];
                 self.isInProgress = NO;
                 [MRProgressOverlayView dismissOverlayForView:[UIApplication sharedApplication].keyWindow animated:YES];
@@ -92,9 +94,9 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([segue.identifier isEqualToString:@"showResult"])
     {
-        Product *product = [DataCache getSelectedItem];
-        product.price = [[self.priceEarned.text stringByReplacingOccurrencesOfString:@"$" withString:@""] floatValue];
-        [DataCache setSelectedItem:product];
+      //  Product *product = [DataCache getSelectedItem];
+       // product.price = [[self.priceEarned.text stringByReplacingOccurrencesOfString:@"$" withString:@""] floatValue];
+        //[DataCache setSelectedItem:product];
     }
 }
 
